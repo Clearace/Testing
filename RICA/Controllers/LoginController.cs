62e39a25fcf504fcb21fcf73b68f39e4173bd958
clearance.baloyi <clearance.baloyi@absa.co.za>
2018-02-19 10:44:56 +0200

    Add project files.

---
+++ RICA/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RICA.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login(string user, string pass)
+        {
+            
+            return View();
+        }
+
+        // GET: Login/Register
+        [HttpGet]
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        // POST: Login/Register
+        [HttpPost]
+        public ActionResult Register(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Login/Reset Password
+        public ActionResult Reset(int id)
+        {
+            return View();
+        }
+
+        // POST: Login/Reset password
+        [HttpPost]
+        public ActionResult Reset(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RICA/Models/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RICA.Models
+{
+    public class Register
+    {
+        public string stuno { get; set; }
+        public string stuname { get; set; }
+        public long stuidno { get; set; }
+        public string stupassword { get; set; }
+        public string stuaddress { get; set; }
+        public string stusurname { get; set; }
+        public string stugender { get; set; }
+    }
+}
